fa6e91268850b75308fb48f62a0f0df89cab6596
ethan-snyder <ems7100@psu.edu>
2025-08-18 13:29:45 -0400

    Changes for calculating CAGR

--- main.py
+++ main.py
@@ -22,22 +22,20 @@ peg_ratio = pe_ratio/expected_eps
 # # Calculate CAGR
 # eps_cagr = (end_eps / start_eps) ** (1 / num_years) - 1
 
-# Fetch the annual income statement DataFrame (columns = years)
-income = data.income_stmt
+# Calculating the 3Y EBITDA CAGR
+income = data.income_stmt# Fetch the annual income statement DataFrame (columns = years)
+print(income)# Display the DataFrame to inspect
+latest_EBITDA = income.loc["EBITDA"].iloc[0]
+prev_EBITDA = income.loc["EBITDA"].iloc[3]
+print(latest_EBITDA)
+print(prev_EBITDA)
 
-# Display the DataFrame to inspect
-print(income)
 
-# Get the most recent reported revenue
-latest_revenue = income.loc["Total Revenue"].iloc[0]  # 0 is most recent column
+# ebitda_cagr = (latest_ebitda / ebitda_3_years_ago) ** (1 / years) - 1
+EBITDA_3Y = (latest_EBITDA / prev_EBITDA) ** (1/3) - 1
+print("EBITDA 3Y CAGR: ", EBITDA_3Y)
 
-# Get the revenue reported 12 months ago (i.e., previous fiscal year)
-prev_revenue = income.loc["Total Revenue"].iloc[1]    # 1 means second most recent
-
-print(latest_revenue)
-print(prev_revenue)
-
-#test
+#prints
 print("Price: ", price)
 print("EPS TTM: ", eps_ttm)
 print("PE ratio: ", pe_ratio)
